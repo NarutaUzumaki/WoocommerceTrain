e4b919d1e0630331ac0c77ccc5cf669e3e420d5a
NarutaUzumaki <orion.nikolaev@gmail.com>
2020-07-30 15:34:20 +0300

    mobile responsive is done(0.1)

--- app/js/common.js
+++ app/js/common.js
@@ -8,3 +8,10 @@ logo.addEventListener('click', function () {
     menu.classList.toggle("show-menu");
     icons.classList.toggle("show-icons");
 });
+
+let filter = document.getElementById("filter");
+let filter_btn = document.getElementById("filter_btn");
+
+filter_btn.addEventListener('click', function () {
+    filter.classList.toggle("show-filter");
+});
